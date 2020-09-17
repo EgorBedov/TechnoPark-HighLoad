509e2c76bc929cb7c72dc1f6b467381937ab1a76
Egor Bedov <bedovegor2203@gmail.com>
2020-09-17 19:36:51 +0300

    Fix

--- src/Files.py
+++ src/Files.py
@@ -9,7 +9,21 @@ from MyConfig import GLOBAL_PATH
 class Files:
     # key - filename
     # value - 'f' || 'd' || 'no'
-    store = dict()
+    store = dict([
+        # ('/httptest/dir1/', 'd'),
+        # ('/httptest/dir2/', 'd'),
+        # ('/httptest/wikipedia_russia_files/', 'd'),
+        ('/httptest/160313.jpg', 'f'),
+        ('/httptest/b16261023.swf', 'f'),
+        ('/httptest/ef35c.jpeg', 'f'),
+        ('/httptest/jquery-1.9.1.js', 'f'),
+        ('/httptest/logo.v2.png', 'f'),
+        ('/httptest/pic_ask.gif', 'f'),
+        ('/httptest/space in name.txt', 'f'),
+        ('/httptest/splash.css', 'f'),
+        ('/httptest/text..txt', 'f'),
+        ('/httptest/wikipedia_russia.html', 'f')
+    ])
 
     # calling os.isfile is a heavy operation
     # consider checking once and then storing the result
@@ -27,7 +41,7 @@ class Files:
 
         full_path = GLOBAL_PATH + filename[:end]
 
-        # print('Path: ', full_path)
+        print('filename:', filename)
 
         if filename.find('/../') != -1:
             # print('invalid path')
@@ -45,7 +59,7 @@ class Files:
                 body = await afp.read()
         elif os.path.isdir(full_path):
             # print('Send directory')
-            Files.store[filename] = 'd'
+            # Files.store[filename] = 'd'
             full_path = full_path+'index.html'
             if os.path.exists(full_path):
                 status = C.HTTP_STATUS_CODE_OK
@@ -67,16 +81,16 @@ class Files:
             status = C.HTTP_STATUS_CODE_OK
             async with AIOFile(full_path, encoding=C.ENCODING) as afp:
                 body = await afp.read()
-        elif value == 'd':
-            new_name = filename+'index.html'
-            new_value = Files.store[new_name]
-            if new_value == 'f':
-                status, body = await Files.from_store(full_path, new_name)
-            elif new_value == 'no':
-                status = C.HTTP_STATUS_CODE_FORBIDDEN
-            else:
-                status = C.HTTP_STATUS_INTERNAL_SERVER_ERROR
+        # elif value == 'd':
+        #     new_name = filename+'index.html'
+        #     new_value = Files.store[new_name]
+        #     if new_value == 'f':
+        #         status, body = await Files.from_store(full_path, new_name)
+        #     elif new_value == 'no':
+        #         status = C.HTTP_STATUS_CODE_FORBIDDEN
+        #     else:
+        #         status = C.HTTP_STATUS_INTERNAL_SERVER_ERROR
         elif value == 'no':
-            status = C.HTTP_STATUS_CODE_FORBIDDEN
+            status = C.HTTP_STATUS_CODE_NOT_FOUND
 
         return status, body
