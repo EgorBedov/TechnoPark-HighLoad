831aad7213b2580762ead4487b7b0ef61252069c
Egor Bedov <bedovegor2203@gmail.com>
2020-09-20 20:19:29 +0300

    Finish with prefork

--- src/Parser.py
+++ src/Parser.py
@@ -1,85 +1,20 @@
-import asyncio
+from email.parser import BytesParser
 
+import constants as C
 from MyConfig import SERVER_NAME
 from utils import *
 
-import constants as C
 
+def parse(raw: bytes):
+    try:
+        request_line, headers_alone = raw.split(b'\r\n', 1)
+    except:
+        return None
+    headers = BytesParser().parsebytes(headers_alone)
 
-class Parser:
-    @staticmethod
-    async def parse_HTTP_request(reader: asyncio.StreamReader):
-        method, path, ver = await Parser.parse_request_line(reader)
-        if method is None or method == '':
-            print('No method')
-            return None
-        print('Parsed request line: ', method, path, ver)
+    method, path, ver = request_line.decode(C.ENCODING).split()
 
-        headers = await Parser.parse_headers(reader)
-        print('Parsed headers: ', headers)
-
-        # body = await Parser.parse_body(reader)
-        # print('Parsed body: ', body)
-
-        return Request(method, path, ver, headers)
-
-    @staticmethod
-    async def parse_request_line(reader: asyncio.StreamReader):
-        raw = await reader.readline()
-
-        print('Raw: ', raw)
-
-        req_line = raw.decode(C.ENCODING)
-        req_line = req_line.rstrip('\r\n')
-        words = req_line.split()
-        # if len(words) != 3:
-        #     raise Exception('Malformed request line')
-
-        print('Words: ', words)
-
-        return words    #['GET', 'path', 'http/1.1']
-
-    @staticmethod
-    async def parse_headers(reader: asyncio.StreamReader) -> dict:
-        headers = []
-        while True:
-            raw = await reader.readline()
-            line = raw.decode(C.ENCODING)
-            # if len(line) > C.MAX_LINE:
-            #     raise Exception('Header line is too long')
-
-
-            if raw in (b'\r\n', b'\n', b''):
-                # завершаем чтение заголовков
-                break
-
-            headers.append(line)
-            # if len(headers) > C.MAX_HEADERS:
-            #     raise Exception('Too many headers')
-
-        # convert array to dict
-        hdict = {}
-        for h in headers:
-            # h = h.decode('iso-8859-1') # here what
-            k, v = h.split(':', 1)
-            hdict[k] = v
-
-        return hdict
-
-    # @staticmethod
-    # async def parse_body(reader: asyncio.StreamReader):
-    #     body = ''
-    #
-    #     print('Start parsing body')
-    #     while not reader.at_eof():
-    #         chunk = await reader.read(8)
-    #         print(chunk)
-    #         if chunk == b'\r\n' or chunk == b'\n' or chunk == b'':
-    #             break
-    #         print('New chunk: ', chunk.decode(C.ENCODING))
-    #         body += chunk.decode(C.ENCODING)
-    #
-    #     return body
+    return Request(method, path, ver, headers)
 
 
 class Request:
@@ -88,6 +23,9 @@ class Request:
         self.path = path
         self.version = version
         self.headers = headers
+
+    def __str__(self):
+        return f'Method: {self.method}\nPath: {self.path}\nVer: {self.version}\n\n'
 
 
 class Response:
@@ -115,6 +53,5 @@ class Response:
 
         if with_body:
             tmp = tmp + self.body
-
 
         return tmp
